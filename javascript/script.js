f52b3b3e339d38afc2adecd6664a7c794e32264e
tagaychr <94014924+tagaychr@users.noreply.github.com>
2024-03-18 04:20:54 +0000

    x

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
 const supabaseUrl = 'https://yryrzmcfkwklywykoevq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyeXJ6bWNma3drbHl3eWtvZXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3MjMxOTUsImV4cCI6MjAyNjI5OTE5NX0.EUo0LxitPyAIL60ZWgNl4J69gSYsQCTXq-U5yrvoBbQ'
 const supabase = createClient(supabaseUrl, supabaseKey)

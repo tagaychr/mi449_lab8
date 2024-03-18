b90142c6522435766832543786ea062627c3fe06
tagaychr <94014924+tagaychr@users.noreply.github.com>
2024-03-18 03:53:52 +0000

    api call to get books

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://yryrzmcfkwklywykoevq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyeXJ6bWNma3drbHl3eWtvZXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3MjMxOTUsImV4cCI6MjAyNjI5OTE5NX0.EUo0LxitPyAIL60ZWgNl4J69gSYsQCTXq-U5yrvoBbQ'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+
+let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+
+for (book in books){
+    let bookList = document.getElementById('books')
+    bookList.innerHTML += `<li>${book.title}</li>`;
+}
